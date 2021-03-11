83db2c8dcf0d08aa4c1da12d1e9fa189bd127833
Ramachandrarao-dev <ramachandraashokit@gmail.com>
2021-03-11 23:47:43 +0530

    HIS-301 related changes

--- Demo.java
+++ Demo.java
@@ -1,5 +1,11 @@
+import java.util.*;
 public class Demo{
+	private Mam<String,Object> cache =  new HashMap<String,Object>();
 	publci void doProcess(){
 		int a = 12345;
+	}
+	//HIS-301 changes
+	public void loadDataToCache(){
+		//logic
 	}
 }
